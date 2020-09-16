3f074719954feecb2db3785ee697d137fee51f76
James A Sutherland <j.a.sutherland@dundee.ac.uk>
2020-09-16 10:15:29 +0000

    Remove unnecessary using directives

--- Tests/BigIntGuessTests.cs
+++ Tests/BigIntGuessTests.cs
@@ -1,9 +1,6 @@
 ﻿using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Text;
 using TypeGuesser;
 using TypeGuesser.Deciders;
 
--- Tests/PerformanceTests.cs
+++ Tests/PerformanceTests.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Globalization;
-using System.Text;
 using NUnit.Framework;
 using TB.ComponentModel;
 using TypeGuesser;
