3895287ac400c73f965e08ec371ba21073adb80d
James A Sutherland <j@sutherland.pw>
2022-06-21 20:26:40 -0500

    Update version for 1.0.3

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 
 [assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
 [assembly: AssemblyProduct("TypeGuesser")]
-[assembly: AssemblyCopyright("Copyright (c) 2018 - 2019")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 - 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.2")]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
