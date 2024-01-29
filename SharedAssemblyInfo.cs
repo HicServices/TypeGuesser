65bfaba363d2b5c69ba5ad8864cc6d0db708324d
James A Sutherland <>
2024-01-29 10:55:46 -0600

    Update version and retag

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Resources;
 
 [assembly: AssemblyCompany("Health Informatics Centre, University of Dundee")]
 [assembly: AssemblyProduct("TypeGuesser")]
-[assembly: AssemblyCopyright("Copyright (c) 2018 - 2023")]
+[assembly: AssemblyCopyright("Copyright (c) 2018 - 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
